8170eea721bf95b7696f5e2cc2a588ea7b01c9fe
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-01 07:19:51 +0100

    feat: add new item in Tòdolist

--- main.js
+++ main.js
@@ -3,14 +3,43 @@ const app = createApp({
     data(){
         return{
             title: 'Todo List',
+
+            todoList: [
+                {
+                    text: 'task1',
+                    done: true,
+                },
+                {
+                    text: 'task2',
+                    done: false,
+                },
+                {
+                    text: 'task3',
+                    done: false,
+                },
+                {
+                    text: 'task4',
+                    done: false,
+                },
+            ],
+            newItem: [
+                {
+                    text: '',
+                    done: false,
+                }
+            ],
         };
     },
     methods: {
+      saveNewItem(){
+        const newItemCopy = { ...this.newItem };
+        this.todoList.push(newItemCopy);
+      }
        
     },
     
     mounted(){
-        console.log('App montata');
+        
         
     },
 });
